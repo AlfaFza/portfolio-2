8a1ddd5f03b84b4b744a71a48f5c7fe206a0389a
Alfi <sssalfi07@gmail.com>
2024-12-27 20:55:31 +0530

    link added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 import { FaLinkedin } from 'react-icons/fa'; 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa'; 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa6'; 'react-icons/fa';
-import { FaSquareTwitter } from 'react-icons/fa6'; 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa6'; 'react-icons/fa';
 
 
 const Navbar = () => {
@@ -12,10 +12,22 @@ const Navbar = () => {
         <img className="mx-2 w-12" src={logo} alt="" />
     </div>
     <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-        <FaLinkedin/>
+        {/* <FaLinkedin/>
         <FaGithub/>
         <FaInstagram/>
-        <FaSquareTwitter/>
+        <FaSquareTwitter/> */}
+         <a href="https://www.linkedin.com/in/alfiya07/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/AlfaFza" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/alf.u.z/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://wa.link/or7iak" target="_blank" rel="noopener noreferrer">
+          <FaWhatsapp />
+        </a>
     </div>
   </nav>
 }
